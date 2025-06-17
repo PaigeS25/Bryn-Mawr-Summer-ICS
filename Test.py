6a76dd34b6953f1ffdccf4925ceed31aea7f2902
PaigeS25 <stevensp@brynmawrschool.org>
2025-06-17 10:44:41 -0400

    Changed files

--- Test.py
+++ Test.py
@@ -1,1 +1,21 @@
 print("Hello World")
+
+print(5 + 10)
+
+number = 10
+
+print(number)
+
+num2 = 5
+
+print(number + num2)
+
+name = "Paige Stevens"
+
+print(name)
+
+number = 10 
+print(number)
+
+number = 25
+print (number)
---
+++ calculator.py
@@ -0,0 +1,22 @@
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+num6 = 6
+num7 = 7
+num8 = 8
+num9 = 9
+num10 = 10
+sum = num1 + num2
+mult = num3 * num4
+divis = num5 / num6
+subt = num8 - num7 
+moddivis = num10 % num9 
+
+print (sum)
+print(mult)
+print(divis)
+print(subt)
+print(moddivis)
+
