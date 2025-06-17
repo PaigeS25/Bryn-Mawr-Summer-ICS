69db5b4b7fa17797170ac36d7989b1c9787797fc
PaigeS25 <stevensp@brynmawrschool.org>
2025-06-17 11:39:41 -0400

    changed files

--- Test.py
+++ Test.py
@@ -19,3 +19,17 @@ print(number)
 
 number = 25
 print (number)
+
+print("demo input")
+
+name = input("what is your name? ")
+
+print(name)
+
+print("input and math") 
+
+num1 = int(input("type in num 1: "))
+num2 = int(input("type in num 2: "))
+sum = num1 + num2
+
+print(f"the sum of {num1} and {num2} is {sum} ")
--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+""" 
 num1 = 1
 num2 = 2
 num3 = 3
@@ -19,4 +20,21 @@ print(mult)
 print(divis)
 print(subt)
 print(moddivis)
+"""
+
+num1 = int(input("enter num1: "))
+num2 = int(input("enter num2: "))
+sum = num1 + num2 
+difference = num1 - num2
+product = num1 * num2
+divis = num1 / num2 
+moddivis = num1 % num2
+
+print(f"the sum of {num1} and {num2} is {sum}")
+print(f"the difference of {num1} and {num2} is {difference}")
+print(f"the product of {num1} and {num2} is {product}")
+print(f"the quotiant of {num1} and {num2} is {divis}") 
+print(f"the modular division of {num1} and {num2} is {moddivis}")
+
+
 
