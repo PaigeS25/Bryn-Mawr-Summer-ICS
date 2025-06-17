9c8eba612d9e86eb040b22cc12701797b00e0c02
PaigeS25 <stevensp@brynmawrschool.org>
2025-06-17 11:51:31 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,40 +1,17 @@
-""" 
-num1 = 1
-num2 = 2
-num3 = 3
-num4 = 4
-num5 = 5
-num6 = 6
-num7 = 7
-num8 = 8
-num9 = 9
-num10 = 10
-sum = num1 + num2
-mult = num3 * num4
-divis = num5 / num6
-subt = num8 - num7 
-moddivis = num10 % num9 
-
-print (sum)
-print(mult)
-print(divis)
-print(subt)
-print(moddivis)
-"""
 
 num1 = int(input("enter num1: "))
 num2 = int(input("enter num2: "))
 sum = num1 + num2 
 difference = num1 - num2
 product = num1 * num2
-divis = num1 / num2 
-moddivis = num1 % num2
+quotient = num1 / num2 
+remainder = num1 % num2
 
 print(f"the sum of {num1} and {num2} is {sum}")
 print(f"the difference of {num1} and {num2} is {difference}")
 print(f"the product of {num1} and {num2} is {product}")
-print(f"the quotiant of {num1} and {num2} is {divis}") 
-print(f"the modular division of {num1} and {num2} is {moddivis}")
+print(f"the quotient of {num1} and {num2} is {quotient}") 
+print(f"the remainder of {num1} and {num2} is {remainder}")
 
 
 
