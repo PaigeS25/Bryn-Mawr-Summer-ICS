fe260242a1bbab78a27b995a18514c1de6ace9b7
PaigeS25 <stevensp@brynmawrschool.org>
2025-06-18 07:32:49 -0400

    Create StudentID.py

---
+++ StudentID.py
@@ -0,0 +1,5 @@
+name = input("enter your name: ")
+gradelevel = input("enter your grade level: ")
+email = input("enter your email: ")
+
+print(f"Hello, {name}. Your grade level is {gradelevel}, and your email is {email}.")
