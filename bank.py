9bceb209c1a097232fc7e51eed5cca58ebc716b9
PaigeS25 <stevensp@brynmawrschool.org>
2025-06-25 11:01:00 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -10,9 +10,15 @@ def closeAccount(name,accounts):
 
 def withdraw(name,amount,accounts):
     if name in accounts.keys():
-        accounts[name] = accounts[name] - amount
+        if amount > accounts[name]:
+            print("amount excedes account balence.")
+            return False 
+        else:
+            accounts[name] = accounts[name] - amount
+            return True 
     else:
         print("Invalid account") 
+        return False 
 
 def deposit(name,amount,accounts):
     if name in accounts.keys():
@@ -22,10 +28,16 @@ def deposit(name,amount,accounts):
 
 def transfer(account_1,account_2,amount,accounts):
     if account_1 in accounts.keys() and account_2 in accounts.keys():
-        withdraw(account_1,amount,accounts)
-        deposit(account_2,amount,accounts) 
+        if withdraw(account_1,amount,accounts) == True:
+            deposit(account_2,amount,accounts) 
+        else:
+            print("Invalid amount")
     else:
         print("One of those accounts is not valid.")
+
+def printAccounts(accounts):
+    for name in accounts: 
+        print(f"Account name: {name}, account balence: {accounts[name]}") 
 
 
 #Program
@@ -40,33 +52,37 @@ def main():
         print("4. Deposit")
         print("5. Transfer")
         print("6. Quit")
-        option = int(input("Enter the number of your option from the list above: "))
-        if option == 1:
-            accountname = input("Enter the name of your account: ")
-            balence = float(input("Enter the account balence: "))
-            createAccount(accountname,balence,accounts)
-            print(accounts) 
-        elif option == 2:
-            accountname = input("Enter the name of the account you would like to close: ")
-            closeAccount(accountname,accounts) 
-            print(accounts) 
-        elif option == 3:
-            accountname = input("Enter the name of your account: ")
-            amount = float(input("Enter the amount you would like to withdraw: "))
-            withdraw(accountname,amount,accounts) 
-            print(accounts) 
-        elif option == 4:
-            accountname = input("Enter the name of your account: ")
-            amount = float(input("Enter the amount you would like to deposit: "))
-            deposit(accountname,amount,accounts) 
-        elif option == 5:
-            account_1 = input("Enter the name of the account you wish to transfer from: ")
-            account_2 = input("Enter the name of the account you would like to transfer to: ")
-            amount = float(input("Enter the amount you would like to transfer: "))
-            transfer(account_1,account_2,amount,accounts)
-            print(accounts) 
-        elif option == 6:
-            print("Thanks for visiting!")
-            check = False
+        option = (input("Enter the number of your option from the list above: "))
+        if option.isnumeric() == True: 
+            if int(option) == 1:
+                accountname = input("Enter the name of your account: ").strip()
+                balence = float(input("Enter the account balence: "))
+                createAccount(accountname,balence,accounts)
+                printAccounts(accounts) 
+            elif int(option) == 2:
+                accountname = input("Enter the name of the account you would like to close: ").strip()
+                closeAccount(accountname,accounts) 
+                printAccounts(accounts) 
+            elif int(option) == 3:
+                accountname = input("Enter the name of your account: ").strip()
+                amount = float(input("Enter the amount you would like to withdraw: "))
+                withdraw(accountname,amount,accounts) 
+                printAccounts(accounts) 
+            elif int(option) == 4:
+                accountname = input("Enter the name of your account: ").strip()
+                amount = float(input("Enter the amount you would like to deposit: "))
+                deposit(accountname,amount,accounts) 
+                printAccounts(accounts)
+            elif int(option) == 5:
+                account_1 = input("Enter the name of the account you wish to transfer from: ").strip()
+                account_2 = input("Enter the name of the account you would like to transfer to: ").strip()
+                amount = float(input("Enter the amount you would like to transfer: "))
+                transfer(account_1,account_2,amount,accounts)
+                printAccounts(accounts) 
+            elif int(option) == 6:
+                print("Thanks for visiting!")
+                check = False
+        else:
+            print("Invalid input")
 
 main() 
