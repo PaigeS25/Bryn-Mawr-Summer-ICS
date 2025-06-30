9929da318b30985373156d226e2f09c735f82aed
PaigeS25 <stevensp@brynmawrschool.org>
2025-06-30 11:31:43 -0400

    Add files via upload

---
+++ sketch_1.ino
@@ -0,0 +1,19 @@
+
+const int LED1 = 13;
+const int LED2 = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED1,OUTPUT); 
+  pinMode(LED2,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,LOW);
+  delay(1000);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,HIGH);
+  delay(1000); 
+}
